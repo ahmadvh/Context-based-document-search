94b9794138bcdf875d850a01e3a4d7e588634e0d
Ahmad Varasteh <ahmad.vh7@gmail.com>
2024-10-09 23:52:17 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate     
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+import os 
+
+load_dotenv()
+
+
+
+
+
+
+# Create the 
+
